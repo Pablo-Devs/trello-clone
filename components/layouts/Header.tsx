dc188a1322cf6a54a824771d2140b4fc2ff274c1
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-19 00:35:20 +0000

    refactor: update styles to fix dropdown content layout

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -7,7 +7,7 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { TrelloLogo } from "@/constants/TrelloLogo";
 import Link from "next/link";
 import { Button } from "../ui/button";
@@ -28,42 +28,66 @@ export default function Header() {
               if (item.type === "dropdown") {
                 const key = item.key;
                 const items = dropdownContent[key];
-                console.log(items);
                 return (
                   <NavigationMenuItem key={key}>
                     <NavigationMenuTrigger className="text-foreground hover:text-primary text-base font-medium data-[state=open]:text-primary">
                       {item.key}
                     </NavigationMenuTrigger>
-                    <NavigationMenuContent className="overflow-visible!">
-                      <ul className="grid grid-cols-3 gap-4 p-6 text-left">
-                        {items.map((menuItem, index) => {
-                          const Icon = menuItem.icon as LucideIcon | null;
-                          return (
-                            <li key={index}>
-                              <NavigationMenuLink asChild>
-                                <Link
-                                  href="/" // Replace with actual links lat
-                                  className="block rounded-lg p-4 hover:bg-accent/50 transition-colors group"
-                                >
-                                  <div className="flex items-center gap-2 mb-1">
-                                    {Icon && (
-                                      <div className="text-primary group-hover:text-primary/90">
-                                        <Icon size={20} aria-hidden="true" />
-                                      </div>
-                                    )}
-                                    <h3 className="text-base font-semibold text-foreground">
-                                      {menuItem.title}
-                                    </h3>
-                                  </div>
-                                  <p className="text-xs text-muted-foreground leading-relaxed">
-                                    {menuItem.description}
-                                  </p>
-                                </Link>
-                              </NavigationMenuLink>
-                            </li>
-                          );
-                        })}
-                      </ul>
+                    <NavigationMenuContent className="overflow-visible! grid grid-cols-[2fr_1fr]">
+                      <div className="px-8 pt-8 pb-19 flex flex-col gap-2">
+                        <h3 className="text-foreground text-base font-bold border-b border-secondary-foreground/90 pb-4 mb-2">
+                          Explore the features that help your team succeed
+                        </h3>
+                        <ul className="grid grid-cols-3 gap-4 text-left">
+                          {items.map((menuItem, index) => {
+                            const Icon = menuItem.icon as LucideIcon | null;
+                            return (
+                              <li key={index}>
+                                <NavigationMenuLink asChild>
+                                  <Link
+                                    href="/"
+                                    className="block rounded-lg p-4 hover:bg-amber-50 transition-colors group"
+                                  >
+                                    <div className="flex items-center gap-2 mb-1">
+                                      {Icon && (
+                                        <div className="text-primary group-hover:text-primary/90">
+                                          <Icon size={20} aria-hidden="true" />
+                                        </div>
+                                      )}
+                                      <h3 className="text-base font-semibold text-muted-foreground">
+                                        {menuItem.title}
+                                      </h3>
+                                    </div>
+                                    <p className="text-xs text-muted-foreground leading-relaxed">
+                                      {menuItem.description}
+                                    </p>
+                                  </Link>
+                                </NavigationMenuLink>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </div>
+                      <div className="px-8 pt-8 pb-19 bg-accent">
+                        <div className="flex flex-col p-4 max-w-[300px] w-full">
+                          <h3 className="text-foreground text-base border-b border-secondary-foreground/90 pb-4 mb-4">
+                            Meet Trello
+                          </h3>
+                          <p className="text-foreground/90 text-xs mb-4">
+                            Trello makes it easy for your team to get work done.
+                            No matter the project, workflow, or type of team,
+                            Trello can help keep things organized. It’s simple –
+                            sign-up, create a board, and you’re off!
+                            Productivity awaits.
+                          </p>
+                          <Link
+                            href=""
+                            className="text-sm text-foreground font-semibold border border-primary hover:bg-blue-100 p-3 max-w-[134px]"
+                          >
+                            Check out Trello
+                          </Link>
+                        </div>
+                      </div>
                     </NavigationMenuContent>
                   </NavigationMenuItem>
                 );
@@ -108,23 +132,18 @@ export default function Header() {
           </Button>
         </SheetTrigger>
         <SheetContent side="right" className="pt-12">
-          <nav className="flex flex-col gap-5 text-lg font-medium">
-            <Link href="/features" className="hover:text-primary">
-              Features
-            </Link>
-            <Link href="/solutions" className="hover:text-primary">
-              Solutions
-            </Link>
-            <Link href="/plans" className="hover:text-primary">
-              Plans
-            </Link>
-            <Link href="/pricing" className="hover:text-primary font-semibold">
-              Pricing
-            </Link>
-            <Link href="/resources" className="hover:text-primary">
-              Resources
-            </Link>
-          </nav>
+          <SheetHeader className="hidden">
+            <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
+            <SheetDescription className="sr-only"></SheetDescription>
+          </SheetHeader>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger></NavigationMenuTrigger>
+                <NavigationMenuContent></NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </SheetContent>
       </Sheet>
     </header>
