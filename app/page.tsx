bbe4cf77991b06c6dfd4a6affe02a25bd7880d5f
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-19 14:59:37 +0000

    feat: integrate hero section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+"use client"
+
+import Hero from "@/components/sections/Hero";
+import Link from "next/link";
+
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello, Trello!</h1>
-    </div>
-  )
-}
+    <main>
+      <div className="bg-chart-1/20 w-full py-4 px-8">
+        <p className="text-base text-foreground/80 font-medium text-center">
+          Accelerate your teams&apos; work with Atlassian Intelligence (AI)
+          features 🤖 now available for all Premium and Enterprise!{" "}
+          <Link href="" className="text-primary underline">
+            Learn more.
+          </Link>
+        </p>
+      </div>
+      {/* Hero Section */}
+      <Hero />
+      
+    </main>
+  );
+};
 
-export default Page
+export default Page;
