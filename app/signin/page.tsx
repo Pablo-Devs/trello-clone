35cdcdfe6fac992ccc7a63e4ed2fcfc9f29abe32
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-20 15:18:48 +0000

    refactor: remove unsed import

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import { Info, SquareArrowOutUpRight } from "lucide-react";
 import Image from "next/image";
 import { Label } from "@/components/ui/label";
