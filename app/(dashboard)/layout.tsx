32a665349c64c417601521375108cc25bdb17f71
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-22 02:29:42 +0000

    refactor: integrate Navbar component into dashbord layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import { Toaster } from "@/components/ui/sonner"
+import Navbar from "@/components/layouts/Navbar"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
+      <Navbar />
         { children }
         <Toaster />
     </main>
