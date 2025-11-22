e09668f9daddd446948d5862635d5e00ecf04c4e
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-22 02:28:10 +0000

    refactor: add user interface to define the shape of user

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,14 @@
 import { LucideIcon } from "lucide-react";
 
-export type AuthProvider = "google" | "azure" | "apple" | "slack"
+export type AuthProvider = "google" | "azure" | "apple" | "slack";
+
+export interface User {
+  email?: string;
+  user_metadata?: {
+    avatar_url?: string;
+    full_name?: string;
+  };
+}
 
 export interface DropdownItem {
   title: string;
