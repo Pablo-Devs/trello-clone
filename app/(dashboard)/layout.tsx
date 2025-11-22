2409e3379bc5cae0f634b1b5b6e42a53e017b2d2
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-22 20:01:45 +0000

    refactor: update layout to integrate SidebarWrapper

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/layouts/Navbar";
-import Sidebar from "@/components/layouts/Sidebar";
+import SidebarWrapper from "@/components/wrappers/sidebar-wrapper";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex flex-col h-screen">
       <Navbar />
       <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
+        <SidebarWrapper />
         <div className="flex-1 overflow-y-auto py-18 pl-18 pr-10">{children}</div>
       </div>
       <Toaster />
